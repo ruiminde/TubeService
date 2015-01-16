fa36b12fdd1bfcdb8e48dfda1460dbc4149e3865
Rui Martins <rui-c-martins@telecom.pt>
2015-01-16 20:55:14 +0000

    Discovered the endpoint that returns the status in JSON. Refactored the code to support this provider.

--- metro_lisboa.py
+++ metro_lisboa.py
@@ -6,8 +6,6 @@ import logging
 import requests
 import requests.exceptions
 
-import html_parser
-
 
 class LineStatus(object):
     LINE_YELLOW = 'yellow'
@@ -21,37 +19,8 @@ class LineStatus(object):
     STATUS_HALT = 'halt'
     STATUS_PARTIAL_HALT = 'partial_halt'
 
-    REASON_UNKNOWN = None
-    REASON_TROUBLES = "There's a flood"
-    REASON_PARTIAL_HALT = "Yo mama's so fat..."
-    REASON_HALT = "No electricity"
-
-    _STATUS_KEYWORDS_MAP = {
-        'Circulação normal': STATUS_OK,
-        'existem perturbações na circulação': STATUS_DELAY,
-        'a circulação encontra-se com perturbações': STATUS_DELAY,
-        'está interrompida a circulação na linha entre as estações': STATUS_PARTIAL_HALT,
-        'está interrompida a circulação.': STATUS_HALT,
-        'a circulação está interrompida': STATUS_HALT,
-        '__NO__MATCH__': STATUS_UNKNOWN
-    }
-
-    _REASON_KEYWORDS_MAP = {
-        'existem perturbações na circulação': REASON_TROUBLES,
-        'a circulação encontra-se com perturbações': REASON_TROUBLES,
-        'está interrompida a circulação na linha entre as estações': REASON_PARTIAL_HALT,
-        'está interrompida a circulação.': REASON_HALT,
-        'a circulação está interrompida': REASON_HALT,
-        '__NO__MATCH__': REASON_UNKNOWN
-    }
-
-    _LINE_NAMES_MAP = {
-        'Linha Azul': LINE_BLUE,
-        'Linha Verde': LINE_GREEN,
-        'Linha Amarela': LINE_YELLOW,
-        'Linha Vermelha': LINE_RED,
-    }
     _http_connector = None
+    _backend = None
     _last_update = None
 
     _status = {
@@ -61,12 +30,13 @@ class LineStatus(object):
         LINE_RED: STATUS_UNKNOWN,
     }
 
-    def __init__(self, http_connector):
+    def __init__(self, http_connector, backend):
         """
 
         :type http_connector: Requests
         """
         self._http_connector = http_connector
+        self._backend = backend
 
     def _update_from_remote_site(self, destination_url):
         """
@@ -83,51 +53,11 @@ class LineStatus(object):
 
             self._last_update = datetime.now()
             logging.debug(response.text)
-            self._status = LineStatus._parse_response(response.text)
+            self._status = self._backend.parse_response(response.text)
 
         except (DownloadPageError, requests.exceptions.RequestException) as exception:
             logging.error("Failed the GET request to {url}".format(url=destination_url))
             logging.error("{0}".format(exception))
-
-    @classmethod
-    def _parse_response(cls, page_content):
-        """
-        Parse the HTML text and return the dictionary with the status of the lines
-        :rtype : dict
-        :param page_content: HTML text to be parsed
-        """
-
-        try:
-            parsed = html_parser.get_line_status(str(page_content))
-
-        except Exception:
-            raise ParseError()
-
-        if not isinstance(parsed, dict):
-            logging.error("Parse failed")
-            raise ParseError()
-
-        result = {cls._LINE_NAMES_MAP[parsed_line]: (
-        cls._match_status_keyword(parsed_status), cls._match_reason_keyword(parsed_status))
-                  for parsed_line, parsed_status in parsed.items()}
-        return result
-
-    @classmethod
-    def _match_status_keyword(cls, status_message):
-        for keyword, status in cls._STATUS_KEYWORDS_MAP.items():
-            if keyword in status_message:
-                return status
-        else:
-            return cls.STATUS_UNKNOWN
-
-    @classmethod
-    def _match_reason_keyword(cls, status_message):
-        for keyword, reason in cls._REASON_KEYWORDS_MAP.items():
-            if keyword in status_message:
-                return reason
-        else:
-            return cls.REASON_UNKNOWN
-
 
     def get_latest(self, destination_url, line=None):
         """
@@ -143,10 +73,6 @@ class LineStatus(object):
             return self._status
         else:
             return {line: self._status[line]}
-
-
-class ParseError(Exception):
-    pass
 
 
 class DownloadPageError(Exception):
