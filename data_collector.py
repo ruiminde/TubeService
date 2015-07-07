48bbd04e8f70fccf7ec5c9b4f8839e6febeef7c7
Rui Martins <rui-c-martins@telecom.pt>
2015-07-07 21:52:23 +0100

    Data collector POST locally

--- data_collector.py
+++ data_collector.py
@@ -7,27 +7,12 @@ import logging
 from datetime import datetime
 
 import requests
-from sqlalchemy.ext.declarative import declarative_base
 
 import settings.config as config
 from tubeservice import metro_lisboa
-from tubeservice.models import LineStatusLog
 
-
-Base = declarative_base()
-
-_backends = {
-    'json': {'url': config.JSON_BACKEND_URL, 'module': 'lib.json_backend'},
-    'html': {'url': config.HTML_BACKEND_URL, 'module': 'lib.html_backend'},
-}
-
-from sqlalchemy import create_engine
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
-from sqlalchemy.orm import sessionmaker
-session = sessionmaker()
-session.configure(bind=engine)
-Base.metadata.create_all(engine)
-s = session()
+_backends = {"json": {'url': config.JSON_BACKEND_URL, 'module': "lib.json_backend"},
+             "html": {'url': config.HTML_BACKEND_URL, 'module': "lib.html_backend"}}
 
 def get_line_status(active_backend_name):
     active_backend = _backends[active_backend_name]
@@ -37,10 +22,6 @@ def get_line_status(active_backend_name):
     metro_lisboa_lines._update_from_remote_site(url)
     logging.warning(metro_lisboa_lines._status)
 
-    # TODO: Get DB context; iterate over all_lines_status; save data
     for line_name, line_status in metro_lisboa_lines._status.items():
-        l = LineStatusLog(line_name, line_status[0], line_status[1], datetime.now())
-        s.add(l)
-        s.commit()
-
-
+        requests.post('http://localhost:5000/status/',
+                      data=dict(line=line_name, status=line_status[0], reason=line_status[1], timestamp=datetime.now()))
